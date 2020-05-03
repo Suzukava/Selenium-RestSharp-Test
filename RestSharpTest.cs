33399d095744427af517d9029377b63bd47aa07c
Suzukava <mah9ibk@gmail.com>
2020-05-03 13:20:19 +0300

    first commit

---
+++ RestSharpTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace selenium_tests
+{
+    [TestFixture]
+    public class RestSharpTest
+    {
+        private string getUrl = "https://docs.microsoft.com/api/search?search=LINQ&locale=ru-ru&scoringprofile=search_for_en_us_a_b_test&facet=category&%24skip=0&%24top=10";
+
+        [OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        public void OneTimeSetUp()
+        {
+            
+        }
+
+        [OneTimeTearDown] //вызывается после завершения всех тестов
+        public void OneTimeTearDown()
+        {
+            
+        }
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
+        {
+
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void TEST_1()
+        {
+            IRestClient restClien = new RestClient();
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+                string getUrl1 = "https://docs.microsoft.com/api/search?search=LINQ&locale=ru-ru&scoringprofile=search_for_en_us_a_b_test&facet=category&%24skip=" + i + "0&%24top=10";
+
+                IRestRequest restReaquest = new RestRequest(getUrl1);
+                restReaquest.AddHeader("Accept", "application/json");
+
+                IRestResponse restResponse = restClien.Get(restReaquest);
+                if (restResponse.IsSuccessful)
+                {
+                    Console.WriteLine(restResponse.StatusCode);
+                    var o = JObject.Parse(restResponse.Content);
+
+                    var result = o["results"];
+                    foreach (var res in result)
+                    {
+                        StringBuilder sb = new StringBuilder((string)res["title"]);
+                        foreach (var desc in res["descriptions"])
+                        {
+                            sb.Append(" ; ");
+                            sb.Append((string)desc["content"]);
+                        }
+                        string s1 = sb.ToString();
+                        string s2 = "linq";
+                        bool b = s1.ToLower().Contains(s2);                       
+
+                        if (b == true)
+                        {
+                            Console.WriteLine($"Result: {b} \n Search result contains sought-for word");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Result: {b} \n Search result not contains sought-for word");
+                        }
+                        Assert.IsTrue(b);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ selenium_demo.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace selenium_tests
+{
+    [TestFixture]
+    public class Test
+    {
+        IWebDriver driver;
+        [OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        public void OneTimeSetUp()
+        {
+            driver = new FirefoxDriver("E:\\WORK\\QA\\selenium");
+            driver.Url = "https://docs.microsoft.com/ru-ru/";
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown] //вызывается после завершения всех тестов
+        public void OneTimeTearDown()
+        {
+            
+        }
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+        [Test]
+        public void TEST_1()
+        {
+            IWebElement searcher = driver.FindElement(By.XPath(".//*[@id='searchFormDesktop']"));
+
+            Actions act = new Actions(driver);
+
+            act.MoveToElement(searcher).Click().Perform();
+            act.MoveToElement(searcher).SendKeys("LINQ");
+            act.MoveToElement(searcher).SendKeys(Keys.Enter).Perform();
+            Thread.Sleep(2000);
+            for (int i = 1; i < 5; i++)
+            {
+
+                IWebElement mainPanel = driver.FindElement(By.Id("main"));
+                IWebElement resultsPanel = mainPanel.FindElement(By.XPath(".//div[@id='search-results']"));
+                ReadOnlyCollection<IWebElement> searchResults = resultsPanel.FindElements(By.XPath(".//li"));              
+                
+                foreach (IWebElement result in searchResults)
+                {
+                    Console.WriteLine("----------------------------------------------------------------");
+                    string s1 = result.Text.ToString();
+                    string s2 = "linq";
+                    bool b = s1.ToLower().Contains(s2);
+                    if(b == true)
+                    {
+                        Console.WriteLine($"Result: {b} \n Search result contains sought-for word");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result: {b} \n Search result not contains sought-for word");
+                    }
+                    //Console.WriteLine(result.Text);
+                    Console.WriteLine("----------------------------------------------------------------");
+                }
+                
+                IWebElement pagePanel = mainPanel.FindElement(By.XPath(".//*[contains(@class,'pagination')]"));
+                Actions action = new Actions(driver);
+                action.SendKeys(Keys.End);
+                action.Perform();
+                IWebElement pager = pagePanel.FindElement(By.XPath(".//a[@href='/ru-ru/search/?search=LINQ&category=All&skip="+i+"0']"));
+                pager.Click();
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
